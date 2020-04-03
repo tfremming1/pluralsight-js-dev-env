abff7c08f7e1b0c33d22167dcd3e3ce35b3f1ab3
travisfremming <33354051+travisfremming@users.noreply.github.com>
2020-04-02 21:23:31 -0500

    webserver items

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -5,14 +5,14 @@ var open = require('open');
 var port = 3000;
 var app = express();
 
-app.get('/', function(req, res) {
-   res.sendFile(path.join(__dirname, '../src/index.html')); 
+app.get('/', function(_req, res) {
+   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 app.listen(port, function(err) {
     if (err) {
         console.log(err);
     } else {
-        open('http//localhost:' + port);
+        open('http://localhost:' + port);
     }
-})
+});
